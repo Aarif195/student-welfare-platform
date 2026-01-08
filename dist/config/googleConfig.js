a79fba51258285267c60a666a733e2d021a6001d
Aarif <adebayoabdulazeez195@gmail.com>
2026-01-08 18:03:39 +0100

    Remove dist from repo and add to gitignore

--- dist/config/googleConfig.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const google_auth_library_1 = require("google-auth-library");
-const client = new google_auth_library_1.OAuth2Client(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URI);
--- dist/controllers/hostelControllers/hostelPublicController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchHostelsController = exports.getAllHostelsController = void 0;
-const getAllHostelsController = (req, res) => { };
-exports.getAllHostelsController = getAllHostelsController;
-const searchHostelsController = (req, res) => { };
-exports.searchHostelsController = searchHostelsController;
